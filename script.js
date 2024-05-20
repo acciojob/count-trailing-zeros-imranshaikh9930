00ae14e4e1029efb8bea78977543b4cdb6e0dcef
imran <87297004+imranshaikh9930@users.noreply.github.com>
2024-05-20 12:14:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 function trailingZeros(n) {
   //your JS code here. If required.
+
+	let temp = 0;
+	let count = 0;
+	while(n >=0){
+		temp = n % 10;
+		count++;
+		n/=10;
+	}
+
+	retunn count;
 }
 
-const input = prompt("Enter a number");
-alert(trailingZeros(input));
+console.log(trailingZeros(10))
+
+// const input = prompt("Enter a number");
+// alert(trailingZeros(input));
